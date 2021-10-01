d26832471fccead8328184bb4157365d6fda8f65
shindeanant <shindeanant1997@gmail.com>
2021-10-01 16:10:27 +0530

    ADD: Setting the initial position as zero

--- SnakeAndLadderAssignment.java
+++ SnakeAndLadderAssignment.java
@@ -1,9 +1,17 @@
 package com.snakeandladderGamee;
 
 public class SnakeAndLadderAssignment {
+	static int position = 0;
+
+	public void startGame() { /** instance method */
+		System.out.println("Position of player at start is " + position);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Snake and Ladder Game:");
+
+		SnakeAndLadderAssignment object = new SnakeAndLadderAssignment(); /** Object creation */
+		object.startGame(); /*** Calling method */
 
 	}
 
