bb6c5a062a51fe3639af64d675d6ef062267dd4e
shindeanant <shindeanant1997@gmail.com>
2021-10-01 16:15:40 +0530

    ADD: Rolling the dice to get a number between 1 to 6

--- SnakeAndLadderAssignment.java
+++ SnakeAndLadderAssignment.java
@@ -1,5 +1,7 @@
 package com.snakeandladderGamee;
 
+import java.util.Random;
+
 public class SnakeAndLadderAssignment {
 	static int position = 0;
 
@@ -7,11 +9,24 @@ public class SnakeAndLadderAssignment {
 		System.out.println("Position of player at start is " + position);
 	}
 
+	void getRandomNumber() {
+
+		Random random = new Random();
+		int randomNumber = 1;
+		if (randomNumber > 0) {
+			/** generating random number between 1 to 6 */
+
+			randomNumber = random.nextInt(7);
+			System.out.println(randomNumber);
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Snake and Ladder Game:");
 
 		SnakeAndLadderAssignment object = new SnakeAndLadderAssignment(); /** Object creation */
 		object.startGame(); /*** Calling method */
+		object.getRandomNumber();
 
 	}
 
