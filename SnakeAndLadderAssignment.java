ddaf0113ee9b98da3afecc81997fb224c010a2f9
shindeanant <shindeanant1997@gmail.com>
2021-10-01 16:22:29 +0530

    ADD: Check option for No play, Ladde or Snake

--- SnakeAndLadderAssignment.java
+++ SnakeAndLadderAssignment.java
@@ -4,20 +4,30 @@ import java.util.Random;
 
 public class SnakeAndLadderAssignment {
 	static int position = 0;
+	static Random random = new Random();
 
-	public void startGame() { /** instance method */
-		System.out.println("Position of player at start is " + position);
+	public static int getDiceRollNumber() { // generating random number between 1 to 6
+		int randomNumber = random.nextInt(6) + 1;
+		return randomNumber;
 	}
 
-	void getRandomNumber() {
-
-		Random random = new Random();
-		int randomNumber = 1;
-		if (randomNumber > 0) {
-			/** generating random number between 1 to 6 */
-
-			randomNumber = random.nextInt(7);
-			System.out.println(randomNumber);
+	void checkOption() {
+		int getRandom = getDiceRollNumber();
+		int randomOption = random.nextInt(3);
+		System.out.println("Number received after rolling dice is" + getRandom);
+		switch (randomOption) {
+		case 0:
+			position = position;
+			System.out.println("Position of player at no play is " + position);
+			break;
+		case 1:
+			position = position + getRandom;
+			System.out.println("Position of player after climbing ladder is " + position);
+			break;
+		case 2:
+			position = position - getRandom;
+			System.out.println("Position of player after stepping on snake  is " + position);
+			break;
 		}
 	}
 
@@ -25,8 +35,8 @@ public class SnakeAndLadderAssignment {
 		System.out.println("Welcome to the Snake and Ladder Game:");
 
 		SnakeAndLadderAssignment object = new SnakeAndLadderAssignment(); /** Object creation */
-		object.startGame(); /*** Calling method */
-		object.getRandomNumber();
+		object.getDiceRollNumber(); /*** Calling method */
+		object.checkOption();
 
 	}
 
