226dea6d195581bda82a5bc184c87be07fbeb22d
shindeanant <shindeanant1997@gmail.com>
2021-10-01 16:04:57 +0530

    ADD: Welcome message

---
+++ SnakeAndLadderAssignment.java
@@ -0,0 +1,10 @@
+package com.snakeandladderGamee;
+
+public class SnakeAndLadderAssignment {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Snake and Ladder Game:");
+
+	}
+
+}
